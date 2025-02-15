07d369b00d94ea05952544d946ee7152b88187e8
Ellis <me@ellis.pw>
2025-02-14 16:56:46 -0800

    lazy solution

--- src/Links.Api/Storage/LinksStorage.cs
+++ src/Links.Api/Storage/LinksStorage.cs
@@ -7,7 +7,12 @@ namespace Links.Api.Storage;
 public class LinksStorage : DbContext {
     public DbSet<LinkEntity> Links { get; set; }
 
-    protected override void OnConfiguring( DbContextOptionsBuilder options ) => options.UseSqlite( $"Data Source=/var/Links.Api/LinksStorage.db" );
+    protected override void OnConfiguring(DbContextOptionsBuilder options) {
+        if(Directory.Exists("/var/Links.Api"))
+            options.UseSqlite($"Data Source=/var/Links.Api/LinksStorage.db");
+        if(Directory.Exists("C:\\etc"))
+            options.UseSqlite($"Data Source=C:\\etc\\LinksStorage.db");
+    }
 
     protected override void OnModelCreating( ModelBuilder mb ) {
         base.OnModelCreating( mb );
